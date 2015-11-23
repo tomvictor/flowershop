6d245006bc69f5bcf7ba56e46bc9a0b5bc35ce7c
Tomvictor <Tomvictor@Toms-MacBook-Air.local>
2015-11-23 12:53:21 +0530

    phone no changed

--- sendemail.php
+++ sendemail.php
@@ -11,7 +11,7 @@
     $message = @trim(stripslashes($_POST['message'])); 
 
     $email_from = $email;
-    $email_to = 'email@email.com';//replace with your email
+    $email_to = 'tom@buildfromzero.com';//replace with your email
 
     $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
